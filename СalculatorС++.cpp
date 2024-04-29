91257d44ba2ad1304c37d65c8b12f1b02ab4960e
ZonTik675 <153392716+ZonTik675@users.noreply.github.com>
2024-04-29 16:29:27 +0300

    GlobalUpdate СalculatorС++.cpp

--- СalculatorС++.cpp
+++ СalculatorС++.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 
 using namespace std;
 
@@ -6,29 +6,35 @@ int main() {
     setlocale(LC_ALL, "Russian");
     char op;
     double num1, num2;
+
+    cout << "Введите оператор (+, -, *, /): ";
     cin >> op;
+
+    cout << "Введите два числа: ";
     cin >> num1 >> num2;
 
-    switch (op) {
-    case '+':
-        cout << "Результат: " << num1 + num2 << endl;
-        break;
-    case '-':
-        cout << "Результат: " << num1 - num2 << endl;
-        break;
-    case '*':
-        cout << "Результат: " << num1 * num2 << endl;
-        break;
-    case '/':
-        if (num2 != 0) {
-            cout << "Результат: " << num1 / num2 << endl;
-        }
-        else {
-            cout << "Ошибка: деление на ноль!" << endl;
-        }
-        break;
-
+    switch(op) {
+        case '+':
+            cout << "Результат: " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << "Результат: " << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << "Результат: " << num1 * num2 << endl;
+            break;
+        case '/':
+            if(num2 != 0) {
+                cout << "Результат: " << num1 / num2 << endl;
+            } else {
+                cout << "Ошибка: деление на ноль!" << endl;
+            }
+            break;
+        default:
+            cout << "Ошибка: Недопустимый оператор!" << endl;
+            break;
     }
 
     return 0;
 }
+
