802d76136d3f599db7de1bf3000ccfd0c255fd69
ZonTik675 <153392716+ZonTik675@users.noreply.github.com>
2024-04-29 16:23:12 +0300

    Add files via upload

---
+++ СalculatorС++.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    char op;
+    double num1, num2;
+    cin >> op;
+    cin >> num1 >> num2;
+
+    switch (op) {
+    case '+':
+        cout << "Результат: " << num1 + num2 << endl;
+        break;
+    case '-':
+        cout << "Результат: " << num1 - num2 << endl;
+        break;
+    case '*':
+        cout << "Результат: " << num1 * num2 << endl;
+        break;
+    case '/':
+        if (num2 != 0) {
+            cout << "Результат: " << num1 / num2 << endl;
+        }
+        else {
+            cout << "Ошибка: деление на ноль!" << endl;
+        }
+        break;
+
+    }
+
+    return 0;
+}
